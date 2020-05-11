cff6bea60131dc134619f2c0cd9dce622df24b8d
jonah <jonahthorpe01@hotmail.co.uk>
2020-05-11 01:40:55 +0100

    update 9

--- app/src/main/java/com/example/climbingworkout/WorkoutsDatabase.java
+++ app/src/main/java/com/example/climbingworkout/WorkoutsDatabase.java
@@ -111,7 +111,7 @@ public abstract class WorkoutsDatabase extends RoomDatabase {
                         workoutExerciseDao.insert(workoutExercise);
 
                     // intermediate
-                    difficulty = new WorkoutDifficulty(3, 1, "Intermediate", "This workout is designed to increase how many pull up you can do. It was created for people who find the beginner pull up too easy but can't do ten pull ups. " +
+                    difficulty = new WorkoutDifficulty(3, 1, "Intermediate", "This workout is designed to increase how many pull up you can do. It was created for people who find the beginner pull up routine too easy but can't do ten pull ups. " +
                             "For the sets of pull ups, do as many as you can up to ten. Once you can do three sets of ten pull ups, you can try the advanced workout.");
                     difficultyDao.insert(difficulty);
                         // exercises
@@ -154,7 +154,7 @@ public abstract class WorkoutsDatabase extends RoomDatabase {
 
 
                     // intermediate
-                    difficulty = new WorkoutDifficulty(6, 2, "Intermediate", "This workout is designed to increase how many push up you can do. It was created for people who find the beginner push up too easy but can't do fifteen push ups. " +
+                    difficulty = new WorkoutDifficulty(6, 2, "Intermediate", "This workout is designed to increase how many push up you can do. It was created for people who find the beginner push up routine too easy but can't do fifteen push ups. " +
                             "For the sets of push ups, do as many as you can up to fifteen. Once you can do three sets of fifteen push ups, you can try the advanced workout.");
                     difficultyDao.insert(difficulty);
                         // exercises
@@ -167,6 +167,7 @@ public abstract class WorkoutsDatabase extends RoomDatabase {
                         workoutExercise = new WorkoutExercise(6, "Narrow Push Ups", 2, "max - 2", Utility.REPS, 120, 4, 0, 0);
                         workoutExerciseDao.insert(workoutExercise);
 
+            // core
             card = new WorkoutCard(3, "Core", "Endurance", "@drawable/sit_up");
             cardDao.insert(card);
                 // beginner
@@ -192,6 +193,7 @@ public abstract class WorkoutsDatabase extends RoomDatabase {
                 difficultyDao.insert(difficulty);
                     // exercises
 
+            // fingers
             card = new WorkoutCard(4, "Fingers", "Endurance", "@drawable/hangboard_end");
             cardDao.insert(card);
                 // beginner
@@ -248,25 +250,171 @@ public abstract class WorkoutsDatabase extends RoomDatabase {
 
 
                 // intermediate
-                difficulty = new WorkoutDifficulty(15, 5, "Intermediate", "This workout is designed to increase your overall strength with pulling movements. It was created for people who find the beginner pull up too easy but can't do five pull ups with 5kg added." +
+                difficulty = new WorkoutDifficulty(15, 5, "Intermediate", "This workout is designed to increase your overall strength with pulling movements. It was created for people who find the beginner pull up routine too easy but can't do five pull ups with 5kg added." +
                         " For the sets of pull ups, do as many as you can up to five with the weight added. Once you can do three sets of five pull ups, you can try the advanced workout. When adding, start with 1kg and gradually move up to 5. " +
                         "If you exercising at home and don't have weights, you can use water bottles.");
                 difficultyDao.insert(difficulty);
                 // exercises
+
+            // push
             card = new WorkoutCard(6, "Push", "Strength", "@drawable/decline_push_up");
             cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(16, 6, "Beginner", "This workout is designed to develop the necessary strength to complete more specific workouts. It was created for people who can only do five or less push ups. " +
+                        "For the sets of push ups, do as many as you can up to five. If you can't do any, you can skip this set but make sure to at least try one push up to view progress and see if you can perform a push up." +
+                        "Once you can do three sets of five push ups, you can try the intermediate workout.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+                    workoutExercise = new WorkoutExercise(16, "Knee Push Ups", 3, "10", Utility.REPS, 120, 1, 0, 0);
+                    workoutExerciseDao.insert(workoutExercise);
+                    workoutExercise = new WorkoutExercise(16, "Push Ups", 3, "max", Utility.REPS, 120, 2, 0, 0);
+                    workoutExerciseDao.insert(workoutExercise);
+                    workoutExercise = new WorkoutExercise(16, "Incline Push Ups", 3, "8", Utility.REPS, 120, 3, 0, 0);
+                    workoutExerciseDao.insert(workoutExercise);
+
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(17, 6, "Advanced", "This workout is designed to increase your overall pushing strength. It is created for people who find doing five push ups with 5 kg added to easy. To progress in this workout," +
+                        " you can gradually increase weight to keep the reps you perform around five. You can also try learn some hard body weight exercises like the handstands and planches. If you exercising at home and don't have weights, you can use water bottles or " +
+                        "partner assistance.");
+                difficultyDao.insert(difficulty);
+                // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(18, 6, "Intermediate", "This workout is designed to increase your overall strength with pushing movements. It was created for people who find the beginner push up routine too easy but can't do five push ups with 5kg added." +
+                        " For the sets of push ups, do as many as you can up to five with the weight added. Once you can do three sets of five push ups, you can try the advanced workout. When adding, start with 1kg and gradually move up to 5. " +
+                        "If you exercising at home and don't have weights, you can use water bottles. You also use partner assistance");
+                difficultyDao.insert(difficulty);
+                // exercises
+
+            // core
             card = new WorkoutCard(7, "Core", "Strength", "@drawable/bruce_lee_dragon_flags");
             cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(19, 7, "Beginner", "This workout is designed to develop the necessary strength to complete more specific workouts. It was created for people who new to core training. " +
+                        "For the timed exercises, try your best to complete the full time but don't worry if you can't." +
+                        "Once you can comfortably complete this workout, you can move on to the intermediate workout.");
+                difficultyDao.insert(difficulty);
+                // exercises
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(20, 7, "Advanced", "This workout is developed for people very comfortable with core training. It is designed to improve how tension on the wall build overall strength of the core." +
+                        " To make this workout harder, you can increase the amount of set and reps you complete. You can also add weight or raise your legs when applicable.");
+                difficultyDao.insert(difficulty);
+                // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(21, 7, "Intermediate", "This workout is developed for people somewhat comfortable with core training. It is designed to improve how tension on the wall build overall strength of the core." +
+                        "Once you can complete this workout comfortably, you can move onto the advanced workout.");
+                difficultyDao.insert(difficulty);
+                // exercises
+
+            // fingers
             card = new WorkoutCard(8, "Finger", "Strength", "@drawable/alexhonnoldhangboard");
+            cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(22, 4, "Beginner", "This workout is designed to develop the necessary strength to complete more specific workouts as well as to get use to hangboarding. It was created for people who new to hangboarding. Use an edge size around 20 mm. " +
+                        "You should use variety grips types through out the workout, such as : 4 fingers crimp, 4 fingers open, front 3 open and back 3 open. If this is too difficult, you can reduce the amount sets you perform as well reducing your weight with a pulley system.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(23, 4, "Advanced", "This workout is developed for people very comfortable with hangboarding. It is designed to help you hold on to smaller holds and steeper angels." +
+                        " Use an edge size around 20 mm. You should use variety grips types through out the workout, such as : 4 fingers crimp, 4 fingers open, front 3 open and back 3 open. To make this harder, you can increase the amount of sets you complete, " +
+                        "as well as adding weight to your body. You can also try different style of strength workouts such as one arm hangs.");
+                difficultyDao.insert(difficulty);
+                    // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(24, 4, "Intermediate", "This workout is developed for people somewhat comfortable with hangboarding. It is designed to help you hold on to smaller holds and steeper angels." +
+                        " Use an edge size around 20 mm. You should use variety grips types through out the workout, such as : 4 fingers crimp, 4 fingers open, front 3 open and back 3 open. Once you can complete this workout comfortably, you can move onto the advanced workout.");
+                difficultyDao.insert(difficulty);
+                    // exercises
 
             // power
-            cardDao.insert(card);
+            // pull
             card = new WorkoutCard(9, "Pull", "Power", "@drawable/campus_board_training");
             cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(25, 9, "Beginner", "This workout is designed to increase you power in pulling motions. It is recommended to be able to complete the intermediate strength workout before starting this. This workout will help with dynamic and big" +
+                        " moves while climbing. If this is to hard, try build some foundation strength with the strength workouts as well as lowering the overall volume. When you can comfortably complete this workout, you can move onto the intermediate one.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(26, 9, "Advanced", "This workout is designed to increase you power in pulling motions. This workout will help with dynamic and big moves while climbing. if this is too easy, you can try adding weights" +
+                        " or experiment with new explosive exercises.");
+                difficultyDao.insert(difficulty);
+                    // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(27, 9, "Intermediate", "This workout is designed to increase you power in pulling motions. This workout will help with dynamic and big moves while climbing. If this is too hard you can try decreasing volume and increasing " +
+                        "rest time. When you can comfortably complete this workout, you can move onto the advanced one.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+
+            // push
             card = new WorkoutCard(10, "Push", "Power", "@drawable/plyometric_pushups");
             cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(25, 9, "Beginner", "This workout is designed to increase you power in pushing motions. It is recommended to be able to complete the intermediate strength workout before starting this. This workout will help with dynamic and big" +
+                        " moves while climbing. If this is to hard, try build some foundation strength with the strength workouts as well as lowering the overall volume. When you can comfortably complete this workout, you can move onto the intermediate one.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(26, 9, "Advanced", "This workout is designed to increase you power in pushing motions. This workout will help with dynamic and big moves while climbing. if this is too easy, you can try adding weights" +
+                        " or experiment with new explosive exercises.");
+                difficultyDao.insert(difficulty);
+                    // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(27, 9, "Intermediate", "This workout is designed to increase you power in pushing motions. This workout will help with dynamic and big moves while climbing. If this is too hard you can try decreasing volume and increasing " +
+                        "rest time. When you can comfortably complete this workout, you can move onto the advanced one.");
+                difficultyDao.insert(difficulty);
+                    // exercises
+            // core
             card = new WorkoutCard(11, "Core", "Power", "@drawable/kettle_bell_swing");
             cardDao.insert(card);
+                // beginner
+                difficulty = new WorkoutDifficulty(25, 9, "Beginner", "This workout is designed to increase you power in your core area. It is recommended to be able to complete the beginner strength workout before starting this. This workout will help with dynamic and big" +
+                        " moves while climbing. If this is to hard, try build some foundation strength with the strength workouts as well as lowering the overall volume. When you can comfortably complete this workout, you can move onto the intermediate one.");
+                difficultyDao.insert(difficulty);
+                // exercises
+
+
+
+                // advanced
+                difficulty = new WorkoutDifficulty(26, 9, "Advanced", "This workout is designed to increase you power iin your core area. This workout will help with dynamic and big moves while climbing. if this is too easy, you can try adding weights" +
+                        " or experiment with new explosive exercises.");
+                difficultyDao.insert(difficulty);
+                // exercise
+
+
+
+                // intermediate
+                difficulty = new WorkoutDifficulty(27, 9, "Intermediate", "This workout is designed to increase you power in your core area. This workout will help with dynamic and big moves while climbing. If this is too hard you can try decreasing volume and increasing " +
+                        "rest time. When you can comfortably complete this workout, you can move onto the advanced one.");
+                difficultyDao.insert(difficulty);
+                // exercises
         });
         }
     };
